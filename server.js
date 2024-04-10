1110e00809aa4eba83d226166abc6ec10f647ed7
sangharsh <sangharshdev69@gmail.com>
2024-04-10 18:48:21 +0530

    added env variables

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ import { config as dotenvConfig } from 'dotenv';
 
 // Load environment variables from .env file
 dotenvConfig();
-
+const envUserName = process.env.MONGODB_USERNAME;
+const envPassWord = process.env.MONGODB_PASSWORD;
 const app = express();
 const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(
+    `mongodb+srv://${envUserName}:${envPassWord}@mainnikedb.jx4pwkk.mongodb.net/stake`
+  )
+  .then(() => console.log("mongodb connected"))
+  .catch((error) => {
+    console.log("mongodb error: ", error);
+  });
 
 // Middleware
 app.use(cors());
