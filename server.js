7d22e435ec2ff3392a9ca86902db50ab5e9402ef
sangharsh <sangharshdev69@gmail.com>
2024-04-12 18:50:17 +0530

    register endpoint added succfully

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import { config as dotenvConfig } from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { config as dotenvConfig } from "dotenv";
 
 // Load environment variables from .env file
 dotenvConfig();
@@ -25,35 +25,53 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Registration Schema
-const registerSchema = new mongoose.Schema ({
+const registerSchema = new mongoose.Schema({
   registerEmail: {
     type: String,
-    unique: true
+    unique: true,
   },
   registerUsername: {
     type: String,
-    unique: true
+    unique: true,
   },
   registerPassword: {
-    type: String
-  }
-})
+    type: String,
+  },
+});
 
 // Registration Model
 const registerModel = mongoose.model("register", registerSchema);
 
-// Registration Endpoint
-app.post("/register", (req, res) => {
-  res.send('<h1>Welcome!</h1>');
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome!</h1>");
 });
 
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome!</h1>');
+// Registration Endpoint
+app.post("/register", async (req, res) => {
+  const { sendRegisterEmail, sendRegisterUsername, sendRegisterPassword } =
+    req.body;
+  try {
+    const userEmailIsTrue = await registerModel.findOne({
+      registerEmail: sendRegisterEmail,
+    });
+    if (userEmailIsTrue) {
+      res.status(200).json({ userExist: "exit" });
+    } else {
+      const addRegister = await registerModel.create({
+        registerEmail: sendRegisterEmail,
+        registerUsername: sendRegisterUsername,
+        registerPassword: sendRegisterPassword,
+      });
+      res.status(200).json({ register: "success", info: addRegister });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error from register" });
+  }
 });
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
